687887b1c2e3d7ce61d94f1bdaec4032f4289a5f
CamBC-Lab <branden.camilo@yahoo.com>
2024-08-30 13:30:54 -0400

    Gave the code complete functionality and added a readme made by the code.

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# ReadMe Generator
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+This project is intend to help its users create a quick and professonial README for a new project
+
+## Installation
+Simply download both the index.js and package.json files to get started.
+
+## Usage
+First make sure you have node installed on your pc and command prompt or applications similar to it also installed. Then after you have node and a command prompt application navigate to where you downloaded the files and open the file directory with command prompt. Within the the file directory type 'node index.js' and the application should fire off and begin asking you questions for creating the README.
+
+## Contribution
+Make sure all code presented pertains to the overall usage of creating README's for the user and dont commit changes with broken code keep up the industry standard as cited in https://www.contributor-covenant.org/
+
+## Tests
+Dont fret if your lost on what to do with your project if u cant answer any of the questions presented by this applicaiton simply skip them and come back another time.
+
+## License
+## License
+This project is licensed under the MIT license.
+
+## Questions
+## Questions
+For any questions, please contact me via [GitHub](https://github.com/CamBC-Lab or my email branden.camilo@yahoo.com).
+   
--- index.js
+++ index.js
@@ -1,13 +1,115 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines for your project.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions for your project.',
+    },
+    {
+        type : 'list',
+        name : 'license',
+        message :  'Choose a license for your project',
+        choices : ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type : 'input',
+        name : 'github',
+        message : 'Enter is your GitHub username?'
+
+    },
+    {
+        type : 'input',
+        name : 'email',
+        message : 'Enter your email address?'
+
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
+
+function getLicenseBadge(license) {
+    if (license === 'None') return '';
+    return `![License](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-blue.svg)`;
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const licenseBadge = getLicenseBadge(answers.license);
+        const licenseSection = answers.license !== 'None' ? `## License\nThis project is licensed under the ${answers.license} license.` : '';
+        const questionsSection = `## Questions\nFor any questions, please contact me via [GitHub](https://github.com/${answers.github} or my email ${answers.email}).`;
+        const readmeContent = `
+# ${answers.title}
+
+${licenseBadge}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contribution
+${answers.contribution}
+
+## Tests
+${answers.tests}
+
+## License
+${licenseSection}
+
+## Questions
+${questionsSection}
+   `;
+        writeToFile('README.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
